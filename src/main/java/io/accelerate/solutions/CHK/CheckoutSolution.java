0880c203d0b54582a79360c20ea89809d4acef5e
sbx_user1051 <sbx_user1051@169.254.3.197>
2026-03-20 03:55:00 +0000

    Fri Mar 20 03:55:00 UTC 2026

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -8,10 +8,10 @@ import java.util.Map;
 public class CheckoutSolution {
 
     private static final Map<String, Integer> unitPrices = new HashMap<>() {{
-        put("A", 50),
-                put("B", 30),
-                put("C", 20),
-                put("D", 15)
+        put("A", 50);
+                put("B", 30);
+                put("C", 20);
+                put("D", 15);
     }};
 
     private static final Map<String, int[]> offers = new HashMap<>() {{
@@ -21,13 +21,14 @@ public class CheckoutSolution {
 
 
     public Integer checkout(String items) {
+
         if (items == null || items.isEmpty()) return -1;
         Map<String, Integer> countMap = new HashMap<>();
 
         for (char c : items.toCharArray()) {
             String item = String.valueOf(c);
             if (!unitPrices.containsKey(item)) {
-                countMap.put(item, countMap.getOrDefault(item, 0) + 1)
+                countMap.put(item, countMap.getOrDefault(item, 0) + 1);
             }
         }
 
@@ -37,48 +38,20 @@ public class CheckoutSolution {
             String item = entry.getKey();
             int quantity = entry.getValue();
 
-            if(offers.containsKey(item)){
-                int[] offer  = offers.get(item);
+            if (offers.containsKey(item)) {
+                int[] offer = offers.get(item);
                 int offerQuantity = offer[0];
                 int offerPrice = offer[1];
 
                 total += (quantity / offerQuantity) * offerPrice;
-                total+=(quantity % offerQuantity) * unitPrices.get(item);
-            }else{
-                total +=quantity * unitPrices.get(item);
+                total += (quantity % offerQuantity) * unitPrices.get(item);
+            } else {
+                total += quantity * unitPrices.get(item);
             }
 
+            return total;
         }
 
 
-        int countA = 0;
-        int countB = 0;
-        int countC = 0;
-        int countD = 0;
-        for (char sku : skus.toCharArray()) {
-            switch (sku) {
-                case 'A':
-                    countA++;
-                    break;
-                case 'B':
-                    countB++;
-                    break;
-                case 'C':
-                    countC++;
-                    break;
-                case 'D':
-                    countD++;
-                    break;
-                default:
-                    return -1;
-            }
-        }
 
-        int total = 0;
-        total += (countA / 3) * 130 + (countA % 3) * 50;
-        total += (countB / 2) * 45 + (countB % 2) * 30;
-        total += countC * 20;
-        total += countD * 15;
-        return total;
-    }
-}
+
