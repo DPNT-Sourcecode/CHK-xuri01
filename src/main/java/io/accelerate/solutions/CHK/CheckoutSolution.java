36c4b0782cf79afc69da8c3b432b37f61fefa530
sbx_user1051 <sbx_user1051@169.254.3.197>
2026-03-20 06:13:00 +0000

    Fri Mar 20 06:13:00 UTC 2026

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -34,19 +34,20 @@ public class CheckoutSolution {
         int total = 0;
 
         int countA = count.getOrDefault('A', 0);
-        total += (countA / 3) * 130 + (countA % 3) * 30;
+        total += ((countA / 3) * 130) + ((countA % 3) * 30);
 
         int countB = count.getOrDefault('B', 0);
-        total+=(countB/2) * 45 + (countB % 2) * 30;
+        total+=((countB/2) * 45) + ((countB % 2) * 30);
 
-        total += count.getOrDefault('C',0) + 20;
+        total += count.getOrDefault('C',0) * 20;
 
-        total+=count.getOrDefault('D',0) + 15;
+        total+=count.getOrDefault('D',0) * 15;
 
         return total;
 
     }
 }
+
 
 
 
