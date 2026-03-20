deda0c2f1a7340d6f175123330c0dc8e6edebf8e
sbx_user1051 <sbx_user1051@169.254.3.197>
2026-03-20 02:49:00 +0000

    Fri Mar 20 02:49:00 UTC 2026

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -5,8 +5,15 @@ import io.accelerate.runner.SolutionNotImplementedException;
 public class CheckoutSolution {
     public Integer checkout(String skus) {
         if(skus == null) return -1;
+        int countA =0;
+        int countB = 0;
+        int countC = 0;
+        int coundD = 0;
+
+        
 
         return null;
     }
 }
+
 
