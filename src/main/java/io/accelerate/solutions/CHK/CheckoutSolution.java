b0f510944f6483dd1d08b5784604ba059777f9fa
sbx_user1051 <sbx_user1051@169.254.3.197>
2026-03-20 03:49:01 +0000

    Fri Mar 20 03:49:01 UTC 2026

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -7,37 +7,49 @@ import java.util.Map;
 
 public class CheckoutSolution {
 
-    private static final Map<String, Integer> unitPrices = new HashMap<>(){{
+    private static final Map<String, Integer> unitPrices = new HashMap<>() {{
         put("A", 50),
-        put("B", 30),
-        put("C", 20),
-        put("D", 15)
+                put("B", 30),
+                put("C", 20),
+                put("D", 15)
     }};
 
-    private static final Map<String, int []> offers = new HashMap<>(){{
+    private static final Map<String, int[]> offers = new HashMap<>() {{
         put("A", new int[]{3, 130});
-        put("B", new int[]{2,45});
+        put("B", new int[]{2, 45});
     }};
-
 
 
     public Integer checkout(String items) {
         if (items == null || items.isEmpty()) return -1;
         Map<String, Integer> countMap = new HashMap<>();
 
-        for(char c: items.toCharArray()){
+        for (char c : items.toCharArray()) {
             String item = String.valueOf(c);
-            if(!unitPrices.containsKey(item)){
-                countMap.put(item, countMap.getOrDefault(item, 0)+1)
+            if (!unitPrices.containsKey(item)) {
+                countMap.put(item, countMap.getOrDefault(item, 0) + 1)
             }
         }
 
-        int total =0;
+        int total = 0;
 
-        for(Map.entry())
+        for (Map.Entry<String, Integer> entry : countMap.entrySet()) {
+            String item = entry.getKey();
+            int quantity = entry.getValue();
+
+            if(offers.containsKey(item)){
+                int[] offer  = offers.get(item);
+                int offerQuantity = offer[0];
+                
+            }
+
+        }
 
 
-        int countA = 0; int countB = 0; int countC = 0; int countD = 0;
+        int countA = 0;
+        int countB = 0;
+        int countC = 0;
+        int countD = 0;
         for (char sku : skus.toCharArray()) {
             switch (sku) {
                 case 'A':
@@ -65,6 +77,7 @@ public class CheckoutSolution {
         return total;
     }
 }
+
 
 
 
