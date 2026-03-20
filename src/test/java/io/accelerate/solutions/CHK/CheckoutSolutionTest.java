44dfa1fa66700be1f5a144541a425b13418997df
sbx_user1051 <sbx_user1051@169.254.3.197>
2026-03-20 03:07:00 +0000

    Fri Mar 20 03:07:00 UTC 2026

--- src/test/java/io/accelerate/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/io/accelerate/solutions/CHK/CheckoutSolutionTest.java
@@ -4,9 +4,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-
 class CheckoutSolutionTest {
 
     private CheckoutSolution checkoutSolution;
@@ -19,6 +16,9 @@ class CheckoutSolutionTest {
     @Test
     public void checkoutSingleItems(){
         Assertions.assertEquals(50, checkoutSolution.checkout("A"), "Single A should cost 50");
+        Assertions.assertEquals(30, checkoutSolution.checkout("B"), "Single B should cost 30");
+        Assertions.assertEquals(20, checkoutSolution.checkout("C"), "Single C should cost 20");
+        Assertions.assertEquals(15, checkoutSolution.checkout("D"), "Single D should cost 15");
     }
 
 }
