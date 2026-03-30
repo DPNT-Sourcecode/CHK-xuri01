b98b07857e4c98a3cb06d50fee49f2ff89c9a599
sbx_user1051 <sbx_user1051@169.254.31.55>
2026-03-30 12:50:56 +0000

    Mon Mar 30 12:50:56 UTC 2026

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -5,11 +5,11 @@ import java.util.Map;
 
 public class CheckoutSolution {
 
-    private static final Map<String, Integer> PRICES = Map.of (
-        "A", 50,
-        "B", 30,
-        "C", 20,
-        "D", 15
+    private static final Map<String, Integer> PRICES = Map.of(
+            "A", 50,
+            "B", 30,
+            "C", 20,
+            "D", 15
     );
 
 
@@ -20,32 +20,36 @@ public class CheckoutSolution {
         Map<String, Integer> itemCounts = new HashMap<>();
 
         for (char c : items.toCharArray()) {
-            String key = String.valueOf(c);
+            String sku = String.valueOf(c);
 
-            if (!PRICES.containsKey(key)) {
+            if (!PRICES.containsKey(sku)) {
                 return -1;
             }
 
-            itemCounts.put(key, itemCounts.getOrDefault(key, 0) + 1);
+            itemCounts.put(sku, itemCounts.getOrDefault(sku, 0) + 1);
         }
 
-        int numberOfEItems  = itemCounts.getOrDefault("E", 0);
-        int freeBItemsFromPromotion  = numberOfEItems  / 2;
+        applyFreeBPromotion(itemCounts);
+        int total = 0;
+        total += calculatePrice
 
-        int numberOfBItems  = itemCounts.getOrDefault("B", 0);
-        itemCounts.put("B", Math.max(0, numberOfBItems  - freeBItemsFromPromotion ));
+        int numberOfEItems = itemCounts.getOrDefault("E", 0);
+        int freeBItemsFromPromotion = numberOfEItems / 2;
 
-        int total =  calculateTotalWithSpecialOffers(itemCounts);
+        int numberOfBItems = itemCounts.getOrDefault("B", 0);
+        itemCounts.put("B", Math.max(0, numberOfBItems - freeBItemsFromPromotion));
+
+        int total = calculateTotalWithSpecialOffers(itemCounts);
         total += itemCounts.getOrDefault("C", 0) * 20;
         total += itemCounts.getOrDefault("D", 0) * 15;
 
         return total;
     }
 
-    private int calculateTotalWithSpecialOffers(Map<String, Integer> count){
+    private int calculateTotalWithSpecialOffers(Map<String, Integer> count) {
         int total = 0;
         int numberOfAItems = count.getOrDefault("A", 0);
-        total += (numberOfAItems/ 3) * 130 + (numberOfAItems % 3) * 50;
+        total += (numberOfAItems / 3) * 130 + (numberOfAItems % 3) * 50;
 
         int numberOfBItems = count.getOrDefault("B", 0);
         total += (numberOfBItems / 2) * 45 + (numberOfBItems % 2) * 30;
@@ -54,5 +58,6 @@ public class CheckoutSolution {
     }
 
 }
+
 
 
