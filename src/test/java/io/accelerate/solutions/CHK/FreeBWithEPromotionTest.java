4c537cec0314dc365a997bd324479c575d04f648
sbx_user1051 <sbx_user1051@169.254.27.123>
2026-03-31 12:24:04 +0000

    Tue Mar 31 12:24:04 UTC 2026

--- src/test/java/io/accelerate/solutions/CHK/FreeBWithEPromotionTest.java
+++ src/test/java/io/accelerate/solutions/CHK/FreeBWithEPromotionTest.java
@@ -2,6 +2,9 @@ package io.accelerate.solutions.CHK;
 
 import org.junit.jupiter.api.BeforeEach;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FreeBWithEPromotionTest {
@@ -13,8 +16,20 @@ class FreeBWithEPromotionTest {
         promotion = new FreeBWithEPromotion();
     }
 
-    void ShouldNotChangeBWhenNoE(){
-        
+    void ShouldNotChangeBWhenNoE() {
+        Map<String, Integer> items = new HashMap<>();
+        items.put("B", 2);
+        promotion.apply(items);
+        assertEquals(2, items.get("B"));
+    }
+
+    void shouldGiveOneFreeBForTwoE(){
+        Map<String, Integer> items = new HashMap<>();
+        items.put("E", 2);
+        items.put("B", 1);
+
+        promotion.apply(items);
+        assertEquals(2, items.get("B"));
     }
 
 }
