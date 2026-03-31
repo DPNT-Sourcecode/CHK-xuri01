7d2c5aa8b59cc05830165b032d6b608f2e74e3e9
sbx_user1051 <sbx_user1051@169.254.27.123>
2026-03-31 14:12:06 +0000

    Tue Mar 31 14:12:06 UTC 2026

--- src/test/java/io/accelerate/solutions/CHK/CheckoutContextTest.java
+++ src/test/java/io/accelerate/solutions/CHK/CheckoutContextTest.java
@@ -1,5 +1,8 @@
 package io.accelerate.solutions.CHK;
 
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,6 +10,18 @@ import static org.junit.jupiter.api.Assertions.*;
 class CheckoutContextTest {
 
     private Map<String, Integer>  items;
-    private CheckoutContext 
+    private CheckoutContext context;
+
+    @BeforeEach
+    void setUp(){
+        items = new HashMap<>();
+        items.put("A", 2);
+        items.put("B", 2);
+        context = new CheckoutContext(items);
+    }
+
+    void shouldReturnCorrectItemCounts(){
+        
+    }
 
 }
