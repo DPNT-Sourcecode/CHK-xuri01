27af8ca50b97f591e49fc0791dd6e882a4faa180
sbx_user1051 <sbx_user1051@169.254.27.123>
2026-03-31 15:21:06 +0000

    Tue Mar 31 15:21:06 UTC 2026

--- src/test/java/io/accelerate/solutions/CHK/DefaultPricingRuleTest.java
+++ src/test/java/io/accelerate/solutions/CHK/DefaultPricingRuleTest.java
@@ -1,5 +1,9 @@
 package io.accelerate.solutions.CHK;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,5 +11,18 @@ import static org.junit.jupiter.api.Assertions.*;
 class DefaultPricingRuleTest {
 
     private DefaultPricingRule pricingRule;
-    private Map<String, Integer> 
+    private Map<String, Integer> items;
+
+    @BeforeEach
+    void setup(){
+        pricingRule = new DefaultPricingRule("C", 20);
+        items = new HashMap<>();
+    }
+
+    @Test
+    void shouldCalculatePriceForSingleItem(){
+        items.put("C", 1);
+        int result = pricingRule.calculate(items);
+        assertEquals(20, result);
+    }
 }
