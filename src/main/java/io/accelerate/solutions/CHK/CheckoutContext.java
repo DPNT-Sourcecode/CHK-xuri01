6805c22053654001d225629515d5e4702cc44395
sbx_user1051 <sbx_user1051@169.254.27.123>
2026-03-31 09:31:46 +0000

    Tue Mar 31 09:31:46 UTC 2026

--- src/main/java/io/accelerate/solutions/CHK/CheckoutContext.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutContext.java
@@ -1,5 +1,17 @@
 package io.accelerate.solutions.CHK;
 
+import java.util.Map;
+
 public class CheckoutContext {
-    private final Map<>
+    private final Map<String, Integer> itemCount;
+    private int total = 0;
+
+    public CheckoutContext(Map<String, Integer> itemCount) {
+        this.itemCount = itemCount;
+    }
+
+    public Map<String, Integer> getItemCounts(){
+        return itemCounts;
+    }
 }
+
