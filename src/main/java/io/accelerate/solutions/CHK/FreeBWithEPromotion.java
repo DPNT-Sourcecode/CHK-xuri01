77728d0962a905350dab428543e6b742dce276c5
sbx_user1051 <sbx_user1051@169.254.27.123>
2026-03-31 08:49:46 +0000

    Tue Mar 31 08:49:46 UTC 2026

--- src/main/java/io/accelerate/solutions/CHK/FreeBWithEPromotion.java
+++ src/main/java/io/accelerate/solutions/CHK/FreeBWithEPromotion.java
@@ -7,7 +7,9 @@ public class FreeBWithEPromotion  implements Promotion{
     public void apply(Map<String, Integer> itemCounts) {
         int numberOfEElements = itemCounts.getOrDefault("E", 0);
         int freeBItems = numberOfEElements / 2;
-        int currentBItems = numberOfEElements / 2;
         int currentBItems = itemCounts.getOrDefault("B", 0);
+        int chargeableBItems = Math.max(0, currentBItems - freeBItems);
+        itemCounts.put("B", chargeableBItems);
     }
 }
+
