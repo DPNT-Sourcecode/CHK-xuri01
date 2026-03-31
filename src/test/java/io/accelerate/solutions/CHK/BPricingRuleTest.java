eb0dd58b3f281e000d0a804712b2cfb3e57ae270
sbx_user1051 <sbx_user1051@169.254.27.123>
2026-03-31 15:42:06 +0000

    Tue Mar 31 15:42:06 UTC 2026

--- src/test/java/io/accelerate/solutions/CHK/BPricingRuleTest.java
+++ src/test/java/io/accelerate/solutions/CHK/BPricingRuleTest.java
@@ -1,9 +1,32 @@
 package io.accelerate.solutions.CHK;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BPricingRuleTest {
 
-    private BPricingRule
+    private BPricingRule pricingRule;
+    private Map<String, Integer> items;
+
+    @BeforeEach
+    void setup(){
+        pricingRule = new BPricingRule();
+        items = new HashMap<>();
+    }
+
+    @ParameterizedTest(name = "B count {0} -> {1}")
+    @BeforeEach({
+            "0,0",
+            "1, 30",
+            ""
+    })
+    void shouldCalculateBPricesCorrectly(){
+
+    }
 
 }
