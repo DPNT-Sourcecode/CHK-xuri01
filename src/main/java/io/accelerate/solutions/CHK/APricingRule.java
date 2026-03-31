8c271621e8d773d24cf1a10bc1893f4624a7aef1
sbx_user1051 <sbx_user1051@169.254.27.123>
2026-03-31 08:52:46 +0000

    Tue Mar 31 08:52:46 UTC 2026

--- src/main/java/io/accelerate/solutions/CHK/APricingRule.java
+++ src/main/java/io/accelerate/solutions/CHK/APricingRule.java
@@ -1,4 +1,17 @@
 package io.accelerate.solutions.CHK;
 
-public class APricingRule implements {
+import java.util.Map;
+
+public class APricingRule implements PricingRule{
+    @Override
+    public int calculate(Map<String, Integer> itemCounts) {
+        int count = itemCounts.getOrDefault("A", 0);
+        int total = (count / 5) * 200;
+        count %=5;
+        total+=(count/3) * 130;
+        count %=3;
+        total +=count * 50;
+        return total;
+    }
 }
+
