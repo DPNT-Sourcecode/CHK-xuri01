d60819fbf619d83da8fb7ad2cccedbd48e4ddf51
sbx_user1051 <sbx_user1051@169.254.10.197>
2026-04-01 14:31:49 +0000

    Wed Apr 01 14:31:49 UTC 2026

--- src/test/java/io/accelerate/solutions/CHK/GroupDiscountPricingRule.java
+++ src/test/java/io/accelerate/solutions/CHK/GroupDiscountPricingRule.java
@@ -1,18 +1,37 @@
 package io.accelerate.solutions.CHK;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
 public class GroupDiscountPricingRule implements PricingRule {
 
     private final List<String> skus;
-    private Map<String, Integer> unitPrices;
+    private final Map<String, Integer> unitPrices;
     private final int groupSize;
-    private final int groupSize;
-    
+    private final int groupPrice;
+
+    public GroupDiscountPricingRule(List<String> skus, Map<String, Integer> unitPrices, int groupSize, int groupPrice) {
+        this.skus = skus;
+        this.unitPrices = unitPrices;
+        this.groupSize = groupSize;
+        this.groupPrice = groupPrice;
+    }
+
     @Override
     public int calculate(Map<String, Integer> itemCounts) {
+        List<Integer> allItems = new ArrayList<>();
+
+        for(String sku: skus){
+            int count = itemCounts.getOrDefault(sku, 0);
+            for(int i=0; i < count; i++){
+                allItems.add(unitPrices.get(sku));
+            }
+        }
+        allItems.sort(Comparator.reverseOrder());
         return 0;
     }
 }
+
 
