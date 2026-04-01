27ac647bd82f3f284da1dacd9f8e65eacd1de0b6
sbx_user1051 <sbx_user1051@169.254.10.197>
2026-04-01 09:40:34 +0000

    Wed Apr 01 09:40:34 UTC 2026

---
+++ src/main/java/io/accelerate/solutions/CHK/BuyXGetYFreePromotion.java
@@ -0,0 +1,16 @@
+package io.accelerate.solutions.CHK;
+
+public class BuyXGetYFreePromotion implements Promotion{
+    private final String triggerSku;
+    private final int triggerQuantity;
+    private final String freeSku;
+
+
+    public BuyXGetYFreePromotion(String triggerSku, int triggerQuantity, String freeSku) {
+        this.triggerSku = triggerSku;
+        this.triggerQuantity = triggerQuantity;
+        this.freeSku = freeSku;
+    }
+
+    public void apply(Map<>)
+}
