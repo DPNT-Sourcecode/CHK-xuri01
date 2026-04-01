ab8b0efc4546f33119fd32a71f0cbd5745dd70b5
sbx_user1051 <sbx_user1051@169.254.10.197>
2026-04-01 09:49:34 +0000

    Wed Apr 01 09:49:34 UTC 2026

--- src/test/java/io/accelerate/solutions/CHK/BuyXGetYFreePromotionTest.java
+++ src/test/java/io/accelerate/solutions/CHK/BuyXGetYFreePromotionTest.java
@@ -2,6 +2,9 @@ package io.accelerate.solutions.CHK;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BuyXGetYFreePromotionTest {
@@ -9,7 +12,16 @@ class BuyXGetYFreePromotionTest {
     @Test
     void shouldApplyPromotion_E_gives_B(){
         var promotion = new BuyXGetYFreePromotion("E", 2, "B");
-        
+
+        Map<String, Integer> items = new HashMap<>();
+        items.put("E", 2);
+        items.put("B", 1);
+
+        promotion.apply(items);
+        assertEquals(0, items.get("B"));
     }
+
+    
+
 
 }
