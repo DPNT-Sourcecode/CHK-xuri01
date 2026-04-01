5add5d921fb72be7d6f10d5c417571eda24c9664
sbx_user1051 <sbx_user1051@169.254.10.197>
2026-04-01 09:22:34 +0000

    Wed Apr 01 09:22:34 UTC 2026

--- src/test/java/io/accelerate/solutions/CHK/FreeMWithNPromotionTest.java
+++ src/test/java/io/accelerate/solutions/CHK/FreeMWithNPromotionTest.java
@@ -1,6 +1,10 @@
 package io.accelerate.solutions.CHK;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,6 +14,17 @@ class FreeMWithNPromotionTest {
     @BeforeEach
     void setUp(){
         promotion = new FreeMWithNPromotion();
+    }
+
+    @Test
+    void shouldNotChangeMWhenLessThanThreeN(){
+        Map<String, Integer> itemCounts = new HashMap<>();
+        itemCounts.put("N", 2);
+        itemCounts.put("M", 3);
+
+        promotion.apply(itemCounts);
+        assertEquals(3, itemCounts.get("M"));
+
     }
 
 }
