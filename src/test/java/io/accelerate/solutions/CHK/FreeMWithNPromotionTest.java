fa06ee42a81da5b12f1ccbd4e21c0b0f00672b5f
sbx_user1051 <sbx_user1051@169.254.10.197>
2026-04-01 09:19:34 +0000

    Wed Apr 01 09:19:34 UTC 2026

--- src/test/java/io/accelerate/solutions/CHK/FreeMWithNPromotionTest.java
+++ src/test/java/io/accelerate/solutions/CHK/FreeMWithNPromotionTest.java
@@ -1,7 +1,15 @@
 package io.accelerate.solutions.CHK;
 
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FreeMWithNPromotionTest {
+    private FreeMWithNPromotion promotion;
+
+    @BeforeEach
+    void setUp(){
+        promotion = new FreeMWithNPromotion();
+    }
 
 }
