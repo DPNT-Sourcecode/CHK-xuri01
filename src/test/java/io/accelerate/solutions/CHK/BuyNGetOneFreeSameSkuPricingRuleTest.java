1492fceb681a74552dfc16b89086f7874a2c59ac
sbx_user1051 <sbx_user1051@169.254.10.197>
2026-04-01 11:01:34 +0000

    Wed Apr 01 11:01:34 UTC 2026

--- src/test/java/io/accelerate/solutions/CHK/BuyNGetOneFreeSameSkuPricingRuleTest.java
+++ src/test/java/io/accelerate/solutions/CHK/BuyNGetOneFreeSameSkuPricingRuleTest.java
@@ -9,10 +9,26 @@ import static org.junit.jupiter.api.Assertions.*;
 class BuyNGetOneFreeSameSkuPricingRuleTest {
 
     @Test
-    void shouldApplyFreeItem(){
+    void shouldApplyFreeItem() {
         var rule = new BuyNGetOneFreeSameSkuPricingRule("F", 10, 3);
         var items = new HashMap<String, Integer>();
-        
+        items.put("F", 3);
+        assertEquals(20, rule.calculate(items));
     }
 
+    @Test
+    void shouldApplyMultipleFreeItems(){
+        var rule = new BuyNGetOneFreeSameSkuPricingRule("F", 10, 3);
+        var items = new HashMap<String, Integer>();
+        items.put("F", 6);
+        assertEquals(40, rule.calculate(items));
+    }
+
+    @Test
+    void shouldHandleNoFreeItems(){
+        var rule = new BuyNGetOneFreeSameSkuPricingRule("F", 10, 3);
+        var items = new HashMap<String, Integer>();
+        items.put("F", 2);
+        assertEquals(20, rule.calculate(items));
+    }
 }
