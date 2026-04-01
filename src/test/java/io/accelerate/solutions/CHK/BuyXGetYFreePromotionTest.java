2043ca6f849667d0694809d77293b470920fea06
sbx_user1051 <sbx_user1051@169.254.10.197>
2026-04-01 09:55:33 +0000

    Wed Apr 01 09:55:33 UTC 2026

--- src/test/java/io/accelerate/solutions/CHK/BuyXGetYFreePromotionTest.java
+++ src/test/java/io/accelerate/solutions/CHK/BuyXGetYFreePromotionTest.java
@@ -23,15 +23,26 @@ class BuyXGetYFreePromotionTest {
 
     @Test
     void shouldApplyPromotion_N_Gives_M() {
-        var promotion = new BuyXGetYFreePromotion("N", 2, "B");
+        var promotion = new BuyXGetYFreePromotion("N", 3, "M");
 
         Map<String, Integer> items = new HashMap<>();
-        items.put("E", 2);
-        items.put("B", 1);
+        items.put("N", 3);
+        items.put("M", 2);
 
         promotion.apply(items);
-        assertEquals(0, items.get("B"));
+        assertEquals(1, items.get("M"));
     }
 
+    @Test
+    void shouldApplyPromotion_R_Gives_Q(){
+        var promotion = new BuyXGetYFreePromotion("N", 3, "M");
+
+        Map<String, Integer> items = new HashMap<>();
+        items.put("R", 3);
+        items.put("Q", 2);
+
+        promotion.apply(items);
+        assertEquals(1, items.get("M"));
+    }
 
 }
