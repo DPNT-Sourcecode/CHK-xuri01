5f5b018c360342477933713f367b65d8a0bf75f1
sbx_user1051 <sbx_user1051@169.254.10.197>
2026-04-01 12:48:37 +0000

    Wed Apr 01 12:48:37 UTC 2026

--- src/main/java/io/accelerate/solutions/CHK/Promotion.java
+++ src/main/java/io/accelerate/solutions/CHK/Promotion.java
@@ -3,13 +3,6 @@ package io.accelerate.solutions.CHK;
 import java.util.Map;
 
 public interface Promotion {
-
     void apply(Map<String, Integer> itemCounts);
-
-    /*default int apply(Map<String, Integer> itemCounts, int currentTotal) {
-        apply(itemCounts);
-        return currentTotal;
-    }*/
-
-
 }
+
