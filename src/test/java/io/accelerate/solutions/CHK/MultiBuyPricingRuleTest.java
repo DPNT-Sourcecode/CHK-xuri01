95412d3eeee0314eecada25ee54cfe188ae72421
sbx_user1051 <sbx_user1051@169.254.10.197>
2026-04-01 10:31:33 +0000

    Wed Apr 01 10:31:33 UTC 2026

--- src/test/java/io/accelerate/solutions/CHK/MultiBuyPricingRuleTest.java
+++ src/test/java/io/accelerate/solutions/CHK/MultiBuyPricingRuleTest.java
@@ -1,11 +1,13 @@
 package io.accelerate.solutions.CHK;
 
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MultiBuyPricingRuleTest {
 
     void shouldApplyMultipleOffersForA(){
-        var rule = new 
+        var rule = new MultiBuyPricingRule("A", 50, Map.of())
     }
 
 }
