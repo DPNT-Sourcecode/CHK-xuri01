4623cea42af0b0a5a67c946940abdfc2a6973add
sbx_user1051 <sbx_user1051@169.254.27.245>
2026-04-04 01:36:23 +0000

    Sat Apr 04 01:36:23 UTC 2026

--- src/main/java/io/accelerate/solutions/CHK/GroupDiscountPricingRule.java
+++ src/main/java/io/accelerate/solutions/CHK/GroupDiscountPricingRule.java
@@ -1,7 +1,6 @@
 package io.accelerate.solutions.CHK;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
@@ -53,59 +52,8 @@ public class GroupDiscountPricingRule implements PricingRule {
 
         return total;
 
-        /*List<Integer> prices = extractPrices(itemCounts);
-        prices.sort(Comparator.reverseOrder());
-        int total = calculateGroupedTotal(prices);
-        consumeItems(itemCounts);
-        return total;
-
-         */
     }
-
-    private List<Integer> extractPrices(Map<String, Integer> itemCounts) {
-        var prices = new ArrayList<Integer>();
-        for (String sku : skus) {
-            int count = itemCounts.getOrDefault(sku, 0);
-
-            if (count == 0) {
-                continue;
-            }
-            Integer price = unitPrices.get(sku);
-
-            if (price == null) {
-                throw new NullPointerException("Missing unit price for SKU: " + sku);
-            }
-
-            for (int i = 0; i < count; i++) {
-                prices.add(price);
-            }
-
-        }
-        return prices;
-    }
-
-    private int calculateGroupedTotal(List<Integer> prices) {
-
-        int total = 0;
-        while (prices.size() >= groupSize) {
-            for (int i = 0; i < groupSize; i++) {
-                prices.remove(0);
-            }
-            total += groupPrice;
-        }
-
-        for (int price : prices) {
-            total += price;
-        }
-        return total;
-    }
-
-    private void consumeItems(Map<String, Integer> itemCounts) {
-        for (String sku : skus) {
-            itemCounts.put(sku, 0);
-        }
-    }
-
 
 }
+
 
